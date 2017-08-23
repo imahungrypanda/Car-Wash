5487d5a1a2466d2591988a7d9146d7caf8237625
Steve Olsen <stevelolsen@gmail.com>
2017-08-23 15:29:38 -0700

    Add validations to Vehicle model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,9 @@
 class Vehicle < ApplicationRecord
+  validates :vehicle_type, :license_plate, presence: true
+  validates :license_plate, length: { is: 7 }
+  validates_exclusion_of :license_plate, :in => ["1111111"]
+
+  def cost
+    self.vehicle_type == 'car' ? 5 : 10
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823220741) do
+ActiveRecord::Schema.define(version: 20170823222551) do
 
   create_table "vehicles", force: :cascade do |t|
-    t.string "type", null: false
+    t.string "vehicle_type", null: false
     t.string "license_plate", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
